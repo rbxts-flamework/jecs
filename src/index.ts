ebde06e186cf57deb5d96200b66d59218b76f0a6
Ukendio <ukendio@gmail.com>
2024-10-06 23:08:18 +0200

    compile out

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,15 @@
+export { query, queryRuntimeVersionOnlyIfYouRuntimeValueIds } from "./query";
 export {
-    has,
-    add,
-    insert,
-    set,
-    get,
-    spawn,
-    despawn,
-    registry,
-    signals,
-    added,
-    removed
-} from "./registry"
-
-export {
-    query,
-    queryRuntimeVersionOnlyIfYouRuntimeValueIds
-} from "./query"
-
-export {
-    useHookState,
-    useThrottle
-} from "./topo"
+	add,
+	added,
+	despawn,
+	get,
+	has,
+	insert,
+	registry,
+	removed,
+	set,
+	signals,
+	spawn,
+} from "./registry";
+export { useHookState, useThrottle } from "./topo";
