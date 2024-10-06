33c411eb6a5d021953076bba9d84209ad6a216bb
christopher-buss <christopher.buss+github@protonmail.com>
2024-10-06 02:36:45 +0100

    style(lint): fix eslint styling

--- src/query.ts
+++ src/query.ts
@@ -1,41 +1,42 @@
-import { Flamework, type Modding } from "@flamework/core";
-import * as ecs from "@rbxts/jecs"
-import { registry, component, type Id } from "./registry"
+import type { Modding } from "@flamework/core";
+import type * as ecs from "@rbxts/jecs";
+
+import { component, type Id, registry } from "./registry";
 
 // Full credits to @fireboltofdeath for all of these types. */
 export interface Without<T> {
-    _flamecs_without: T;
+	_flamecs_without: T;
 }
 export interface With<T> {
-    _flamework_with: T;
+	_flamework_with: T;
 }
 
 type Skip<T extends Array<unknown>> = T extends [unknown, ...infer R] ? R : [];
 
 interface Bounds {
-    query: Array<unknown>;
-    with: Array<unknown>;
-    without: Array<unknown>;
+	query: Array<unknown>;
+	with: Array<unknown>;
+	without: Array<unknown>;
 }
 type BoundsTuple<T> = T extends { length: number } & ReadonlyArray<unknown> ? T : [];
 type PushBound<B extends Bounds, K extends keyof B, V> = Omit<B, K> &
-    Record<
-        K,
-        V extends ReadonlyArray<unknown> ? [...BoundsTuple<B[K]>, ...V] : [...BoundsTuple<B[K]>, V]
-    >;
+	Record<
+		K,
+		V extends ReadonlyArray<unknown> ? [...BoundsTuple<B[K]>, ...V] : [...BoundsTuple<B[K]>, V]
+	>;
 
 type Calculate<T extends Array<unknown>, B extends Bounds = Bounds> = T extends []
-    ? { [k in keyof B]: BoundsTuple<B[k]> }
-    : T[0] extends Without<infer V>
-    ? Calculate<Skip<T>, PushBound<B, "without", V>>
-    : T[0] extends With<infer V>
-    ? Calculate<Skip<T>, PushBound<B, "with", V>>
-    : Calculate<Skip<T>, PushBound<B, "query", T[0]>>;
+	? { [k in keyof B]: BoundsTuple<B[k]> }
+	: T[0] extends Without<infer V>
+		? Calculate<Skip<T>, PushBound<B, "without", V>>
+		: T[0] extends With<infer V>
+			? Calculate<Skip<T>, PushBound<B, "with", V>>
+			: Calculate<Skip<T>, PushBound<B, "query", T[0]>>;
 type ToIds<T> = T extends []
-    ? undefined
-    : Modding.Many<{
-        [k in keyof T]: Modding.Generic<T[k], "id">;
-    }>;
+	? undefined
+	: Modding.Many<{
+			[k in keyof T]: Modding.Generic<T[k], "id">;
+		}>;
 /**
  * A World contains entities which have components. The World is queryable and
  * can be used to get entities with a specific set of components.
@@ -50,64 +51,68 @@ type ToIds<T> = T extends []
  * @metadata macro
  */
 interface Query {
-    /** @metadata macro */
-    <T extends Array<unknown>>(
-        terms?: ToIds<Calculate<T>["query"]>,
-        filterWithout?: ToIds<Calculate<T>["without"]>,
-        filterWith?: ToIds<Calculate<T>["with"]>,
-    ): QueryHandle<Reconstruct<Calculate<T>["query"]>>;
+	/** @metadata macro */
+	<T extends Array<unknown>>(
+		terms?: ToIds<Calculate<T>["query"]>,
+		filterWithout?: ToIds<Calculate<T>["without"]>,
+		filterWith?: ToIds<Calculate<T>["with"]>,
+	): QueryHandle<Reconstruct<Calculate<T>["query"]>>;
 
-    rt: <U extends Array<ecs.Entity>>(...args: U) => ecs.Query<ecs.InferComponents<U>>;
+	rt: <U extends Array<ecs.Entity>>(...args: U) => ecs.Query<ecs.InferComponents<U>>;
 }
 
-/** @metadata macro */
+/**
+ * @param terms
+ * @param filterWithout
+ * @param filterWith
+ * @metadata macro
+ */
 export function query<T extends Array<unknown>>(
-    terms?: ToIds<Calculate<T>["query"]>,
-    filterWithout?: ToIds<Calculate<T>["without"]>,
-    filterWith?: ToIds<Calculate<T>["with"]>,
+	terms?: ToIds<Calculate<T>["query"]>,
+	filterWithout?: ToIds<Calculate<T>["without"]>,
+	filterWith?: ToIds<Calculate<T>["with"]>,
 ): QueryHandle<Reconstruct<Calculate<T>["query"]>> {
-    assert(terms !== undefined);
-    const ids = new Array<number>();
-    for (const key of terms) {
-        const id = component(key);
-        ids.push(id);
-    }
+	assert(terms !== undefined);
+	const ids = new Array<number>();
+	for (const key of terms) {
+		const id = component(key);
+		ids.push(id);
+	}
 
-    let result = registry.query(...ids);
-    if (filterWithout !== undefined) {
-        const filterWithoutIds = new Array<number>();
-        for (const key of filterWithout) {
-            const id = component(key);
-            filterWithoutIds.push(id);
-        }
+	let result = registry.query(...ids);
+	if (filterWithout !== undefined) {
+		const filterWithoutIds = new Array<number>();
+		for (const key of filterWithout) {
+			const id = component(key);
+			filterWithoutIds.push(id);
+		}
 
-        result = result.without(...filterWithoutIds);
-    }
+		result = result.without(...filterWithoutIds);
+	}
 
-    if (filterWith !== undefined) {
-        const filterWithIds = new Array<number>();
-        for (const key of filterWith) {
-            const id = component(key);
-            filterWithIds.push(id);
-        }
+	if (filterWith !== undefined) {
+		const filterWithIds = new Array<number>();
+		for (const key of filterWith) {
+			const id = component(key);
+			filterWithIds.push(id);
+		}
 
-        result = result.with(...filterWithIds);
-    }
+		result = result.with(...filterWithIds);
+	}
 
-    return result as never
+	return result as never;
 }
 
 export function queryRuntimeVersionOnlyIfYouRuntimeValueIds<T extends Array<number>>(
-    ...ids: T
-) {
-    return registry.query(...ids)
+	...ids: T
+): ecs.Query<ecs.InferComponents<T>> {
+	return registry.query(...ids);
 }
-
 
 type DynamicBundle = Array<Id<unknown>>;
 
 type QueryIter<T extends Array<unknown>> = IterableFunction<LuaTuple<[number, ...T]>>;
 
 type QueryHandle<T extends Array<unknown>> = {
-    without: (this: QueryHandle<T>, ...exclude: DynamicBundle) => QueryHandle<T>;
+	without: (this: QueryHandle<T>, ...exclude: DynamicBundle) => QueryHandle<T>;
 } & QueryIter<T>;
--- src/topo.ts
+++ src/topo.ts
@@ -1,81 +1,81 @@
-import { Flamework, type Modding } from "@flamework/core";
+import type { Modding } from "@flamework/core";
 
 interface State {
-    cleanup: Callback;
-    state: Record<string, unknown>;
+	cleanup: Callback;
+	state: Record<string, unknown>;
 }
 
 interface StackFrame {
-    node: Record<string, State>;
+	node: Record<string, State>;
 }
 
 const stack: Array<StackFrame> = [];
 
 function addStackFrame(node: Record<string, State>): void {
-    const frame: StackFrame = {
-        node,
-    };
-    stack.push(frame);
+	const frame: StackFrame = {
+		node,
+	};
+	stack.push(frame);
 }
 
 function popStackFrame(): void {
-    stack.pop();
+	stack.pop();
 }
 
 function cleanupAll(): void {
-    const current = stack[stack.size() - 1]!;
+	const current = stack[stack.size() - 1]!;
 
-    for (const [key] of pairs(current.node)) {
-        const state = current.node[key]!;
-        for (const [discriminator] of pairs(state.state)) {
-            state.cleanup(state.state[discriminator]);
-        }
-    }
+	for (const [key] of pairs(current.node)) {
+		const state = current.node[key]!;
+		for (const [discriminator] of pairs(state.state)) {
+			state.cleanup(state.state[discriminator]);
+		}
+	}
 }
 
 export function start(node: Record<string, State>, func: () => void): void {
-    addStackFrame(node);
-    func();
-    cleanupAll();
-    popStackFrame();
+	addStackFrame(node);
+	func();
+	cleanupAll();
+	popStackFrame();
 }
 
 // eslint-disable-next-line ts/explicit-function-return-type -- Returns unknown.
 export function useHookState(
-    key: string,
-    discriminator: unknown,
-    callback: (state: unknown) => void,
+	key: string,
+	discriminator: unknown,
+	callback: (state: unknown) => void,
 ) {
-    const current = stack[stack.size() - 1]!;
-    let storage = current.node[key];
-    if (!storage) {
-        storage = { cleanup: callback, state: {} };
-        current.node[key] = storage;
-    }
+	const current = stack[stack.size() - 1]!;
+	let storage = current.node[key];
+	if (!storage) {
+		storage = { cleanup: callback, state: {} };
+		current.node[key] = storage;
+	}
 
-    discriminator ??= key;
-    const stringifiedKey = tostring(discriminator);
+	discriminator ??= key;
+	const stringifiedKey = tostring(discriminator);
 
-    let state = storage.state[stringifiedKey];
-    if (state === undefined) {
-        state = {};
-        storage.state[stringifiedKey] = state;
-    }
+	let state = storage.state[stringifiedKey];
+	if (state === undefined) {
+		state = {};
+		storage.state[stringifiedKey] = state;
+	}
 
-    return state;
+	return state;
 }
 
 interface Storage {
-    expiry: number;
+	expiry: number;
 }
 
 function cleanup(storage: Storage): boolean {
-    return os.clock() < storage.expiry;
+	return os.clock() < storage.expiry;
 }
 
 interface ThrottleStorage {
-    expiry?: number;
-    time?: number;
+	expiry?: number;
+	time?: number;
 }
 
 const STABLE_DISCRIMINATOR = {};
@@ -94,23 +94,23 @@ const STABLE_DISCRIMINATOR = {};
  * @metadata macro
  */
 export function useThrottle(
-    seconds: number,
-    discriminator?: unknown,
-    key?: Modding.Caller<"uuid">,
+	seconds: number,
+	discriminator?: unknown,
+	key?: Modding.Caller<"uuid">,
 ): boolean {
-    assert(key);
+	assert(key);
 
-    const storage = useHookState(
-        key,
-        discriminator ?? STABLE_DISCRIMINATOR,
-        cleanup as never,
-    ) as ThrottleStorage;
+	const storage = useHookState(
+		key,
+		discriminator ?? STABLE_DISCRIMINATOR,
+		cleanup as never,
+	) as ThrottleStorage;
 
-    if (storage.time === undefined || os.clock() - storage.time >= seconds) {
-        storage.time = os.clock();
-        storage.expiry = os.clock() + seconds;
-        return true;
-    }
+	if (storage.time === undefined || os.clock() - storage.time >= seconds) {
+		storage.time = os.clock();
+		storage.expiry = os.clock() + seconds;
+		return true;
+	}
 
-    return false;
+	return false;
 }
