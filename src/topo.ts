81e516402566cc549aaff5296d28068ab8508aa1
hautajoki <hautajoki.leevi@gmail.com>
2024-10-07 23:23:56 -0700

    doc comments for top

--- src/topo.ts
+++ src/topo.ts
@@ -23,6 +23,14 @@ function cleanupAll(): void {
 	}
 }
 
+/**
+ * Starts a new stack frame for a function, ensuring cleanup after execution.
+ * Intended to be used in systems.
+ *
+ * @param node - The node to store the state for the current function.
+ * @param func - The function to execute within the new stack frame.
+ * @returns - The function to execute within the new stack frame.
+ */
 export function start(node: Map<string, State<unknown>>, func: () => void): void {
 	stack.push({ node });
 	func();
@@ -30,6 +38,14 @@ export function start(node: Map<string, State<unknown>>, func: () => void): void
 	stack.pop();
 }
 
+/**
+ * Creates or retrieves a state object for a hook, keyed by a unique identifier.
+ *
+ * @param key A unique string identifier for the hook state.
+ * @param discriminator An optional value to further distinguish different states within the same key. Defaults to the key itself.
+ * @param cleanup A function that determines whether the state should be cleaned up. It should return true if the state should be removed.
+ * @returns The state object of type T.
+ */
 export function useHookState<T>(key: string, discriminator: unknown = key, cleanup: Cleanup<T>): T {
 	const current = stack[stack.size() - 1]!;
 	let storage = current.node.get(key) as State<T> | undefined;
